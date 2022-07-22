af6c931f65ddd2c907e7801416d2df65cb50b4f4
sushma <sushma@gmail.com>
2022-07-22 09:05:23 +0000

    all files developed

---
+++ src/main/java/jaya/WelcomeService.java
@@ -0,0 +1,17 @@
+package jaya;
+
+import org.eiichiro.gig.Service;
+import org.eiichiro.jaguar.inject.Inject;
+import org.eiichiro.jaguar.inject.Name;
+
+@Name("jaya.WelcomeService")
+@Service
+public class WelcomeService {
+
+	@Inject private WelcomeRepository repository;
+	
+	public Welcome get() {
+		return repository.get();
+	}
+	
+}
